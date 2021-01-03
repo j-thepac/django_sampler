0eb42a4c9ff6826940f58ac20b6423804ec7f688
Deepak <j.thepac@gmail.com>
2021-01-03 08:52:15 +0530

    Update settings.py

--- Sampler/Main/settings.py
+++ Sampler/Main/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@(-8lo-x8evbikx16pum52zxi$!xt+e)&6@9l)nsh0^ojoif2@'
+SECRET_KEY = 'removed due to security reasons'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
